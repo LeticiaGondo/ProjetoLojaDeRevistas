ae1c6915f6db3b1fce6c0bec31cb2f817d15d9d2
leticia gondo <legondo@outlook.com>
2020-04-13 23:13:26 -0300

    criação da model Cliente e model Endereço

---
+++ LojaDeRevistas/LojaDeRevistas/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaDeRevistas
+{
+    class Address
+    {
+        public string streat { get; set; }
+
+        public int number { get; set; }
+
+        public int cpf { get; set; }
+    }
+}
---
+++ LojaDeRevistas/LojaDeRevistas/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaDeRevistas
+{
+    class Client
+    {
+        public string name { get; set; }
+
+        public int ages { get; set; }
+
+        public string cpf { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+
+        public Address address { get; set; }
+    }
+}
